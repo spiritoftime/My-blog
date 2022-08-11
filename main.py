4b08942541dfad4eaaf32e17252fcc2abb44bb1d
choo liang yi <chooliangyi6@gmail.com>
2022-08-11 22:30:10 +0800

    Fix Bug

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("uri", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
